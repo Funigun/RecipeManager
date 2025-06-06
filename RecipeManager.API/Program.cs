13d2ff115755314d1cac180c61caf8296f681b4f
Patryk <stepnikpa@gmail.com>
2025-06-06 20:31:03 +0200

    Setup scalar

--- RecipeManager.API/Program.cs
+++ RecipeManager.API/Program.cs
@@ -1,4 +1,5 @@
 using RecipeManager.ServiceDefaults;
+using Scalar.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -6,13 +7,21 @@ builder.AddServiceDefaults();
 
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("Recipe Manager API")
+                   .WithTheme(ScalarTheme.DeepSpace)
+                   .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+
+        options.Servers = [new("https://localhost:7001")];
+    });
 }
 
 app.UseHttpsRedirection();
--- RecipeManager.Identity.API/Program.cs
+++ RecipeManager.Identity.API/Program.cs
@@ -1,4 +1,5 @@
 using RecipeManager.ServiceDefaults;
+using Scalar.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -6,14 +7,21 @@ builder.AddServiceDefaults();
 
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("Recipe Manager Identity API")
+                   .WithTheme(ScalarTheme.Kepler)
+                   .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+
+        options.Servers = [new("https://localhost:7002")];
+    });
 }
 
 app.UseHttpsRedirection();
