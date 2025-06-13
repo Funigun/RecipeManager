f202d73f14808d532419ff1eb66e5e95369beed1
Patryk <stepnikpa@gmail.com>
2025-06-13 23:38:48 +0200

    Add login form

---
+++ RecipeManager.Shared.Contracts/User/Login/UserLoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeManager.Shared.Contracts.User.Login;
+
+public sealed class UserLoginModel
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
--- RecipeManager.Shared.Contracts/User/Registration/UserRegistrationDto.cs
+++ RecipeManager.Shared.Contracts/User/Registration/UserRegistrationDto.cs
@@ -8,14 +8,3 @@ public sealed record UserRegistrationDto
 
     public string Email { get; init; } = string.Empty;
 }
-
-public sealed class UserRegistrationModel
-{
-    public string UserName { get; set; } = string.Empty;
-
-    public string Password { get; set; } = string.Empty;
-
-    public string ConfirmationPassword { get; set; } = string.Empty;
-
-    public string Email { get; set; } = string.Empty;
-}
