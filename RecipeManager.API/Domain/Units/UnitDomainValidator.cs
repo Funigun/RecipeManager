a69232f8a5fbfc39dcf884cb22d56ad16c912b77
Patryk <stepnikpa@gmail.com>
2025-07-01 00:36:15 +0200

    Add Architecture tests, UnitDomainValidator and update Unit DB configuration

---
+++ RecipeManager.API/Domain/Units/UnitDomainValidator.cs
@@ -0,0 +1,14 @@
+﻿using RecipeManager.API.Domain.Common.Abstractions;
+
+namespace RecipeManager.API.Domain.Units;
+
+public class UnitDomainValidator : IDomainModelValidator<Unit>
+{
+    public const int UnitNameMaxLength = 50;
+    public const int UnitShortNameMaxLength = 25;
+
+    public void Validate(Unit entity)
+    {
+        throw new NotImplementedException();
+    }
+}
--- RecipeManager.API/Persistance/Configuration/UnitsConfiguration.cs
+++ RecipeManager.API/Persistance/Configuration/UnitsConfiguration.cs
@@ -11,5 +11,13 @@ public class UnitsConfiguration : IEntityTypeConfiguration<Unit>
         builder.Property(unit => unit.Id)
                .HasConversion(id => id.Value, value => new UnitId(value))
                .ValueGeneratedOnAdd();
+
+        builder.Property(unit => unit.Name)
+               .HasMaxLength(UnitDomainValidator.UnitNameMaxLength)
+               .IsRequired(true);
+
+        builder.Property(unit => unit.ShortName)
+               .HasMaxLength(UnitDomainValidator.UnitShortNameMaxLength)
+               .IsRequired(false);
     }
 }
