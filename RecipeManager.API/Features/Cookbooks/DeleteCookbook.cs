eb7c420855520cfa9863831f78f9a75009f5821d
Patryk <stepnikpa@gmail.com>
2025-08-04 21:55:37 +0200

    Init feature files for recipes, ingredients and cookbooks

---
+++ RecipeManager.API/Features/Cookbooks/DeleteCookbook.cs
@@ -0,0 +1,5 @@
+﻿namespace RecipeManager.Api.Features.Cookbooks;
+
+public class DeleteCookbook
+{
+}
---
+++ RecipeManager.API/Features/Recipes/UpdateRecipe.cs
@@ -0,0 +1,5 @@
+﻿namespace RecipeManager.Api.Features.Recipes;
+
+public class UpdateRecipe
+{
+}
