616058c2bb19e1707f35591763460d55846880f9
Patryk <stepnikpa@gmail.com>
2025-08-21 21:22:31 +0200

    Init GetRecipeById, GetRecipes and UpdateRecipe tests

--- RecipeManager.Tests/CoreApi/Recipes/GetRecipeByIdTests.cs
+++ RecipeManager.Tests/CoreApi/Recipes/GetRecipeByIdTests.cs
@@ -1,4 +1,11 @@
-﻿using RecipeManager.Integration.Tests.CoreApi.TestFixtures;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using RecipeManager.Api.Domain.Recipes;
+using RecipeManager.Api.Features.Recipes;
+using RecipeManager.Api.Shared.Hateoas.Models;
+using RecipeManager.Integration.Tests.Common.Users;
+using RecipeManager.Integration.Tests.CoreApi.TestFixtures;
 
 namespace RecipeManager.Integration.Tests.CoreApi.Recipes;
 
@@ -7,5 +14,23 @@ public sealed class GetRecipeByIdTests : BaseIntegrationTest
 {
     public GetRecipeByIdTests(WebApiFactory webApiFactory) : base(webApiFactory)
     {
+    }
+
+    [Fact]
+    public async Task GetRecipeById_ShouldReturnRecipe_ForValidId()
+    {
+        // Arrange
+        HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TokenMockFactory.GenerateJwtToken(UserMockFactory.CreateMockedAdmin()));
+        RecipeId recipeId = (await DbContext.Recipes.AsNoTracking().FirstAsync(recipe => recipe.Title == "Existing recipe", CancellationToken.None)).Id.Value;
+
+        // Act
+        HttpResponseMessage response = await HttpClient.GetAsync($"/api/recipes/{recipeId}", CancellationToken.None);
+        string responseBody = await response.Content.ReadAsStringAsync();
+        HateoasResponse<GetRecipeById.Response>? recipeResponse = JsonSerializer.Deserialize<HateoasResponse<GetRecipeById.Response>>(responseBody, JsonOptions);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(recipeResponse);
     }
 }
--- RecipeManager.Tests/CoreApi/Recipes/GetRecipesTests.cs
+++ RecipeManager.Tests/CoreApi/Recipes/GetRecipesTests.cs
@@ -1,4 +1,9 @@
-﻿using RecipeManager.Integration.Tests.CoreApi.TestFixtures;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using RecipeManager.Api.Features.Recipes;
+using RecipeManager.Api.Shared.Hateoas.Models;
+using RecipeManager.Integration.Tests.Common.Users;
+using RecipeManager.Integration.Tests.CoreApi.TestFixtures;
 
 namespace RecipeManager.Integration.Tests.CoreApi.Recipes;
 
@@ -7,5 +12,21 @@ public sealed class GetRecipesTests : BaseIntegrationTest
 {
     public GetRecipesTests(WebApiFactory webApiFactory) : base(webApiFactory)
     {
+    }
+
+    [Fact]
+    public async Task GetRecipes_ShouldReturnAllRecipes_WhenNoFiltersApplied()
+    {
+        // Arrange
+        HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", TokenMockFactory.GenerateJwtToken(UserMockFactory.CreateMockedAdmin()));
+
+        // Act
+        HttpResponseMessage response = await HttpClient.GetAsync("/api/recipes", CancellationToken.None);
+        string responseBody = await response.Content.ReadAsStringAsync();
+        HateoasResponse<GetRecipes.Response> recipesResponse = JsonSerializer.Deserialize<HateoasResponse<GetRecipes.Response>>(responseBody, JsonOptions);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.NotNull(recipesResponse);
     }
 }
