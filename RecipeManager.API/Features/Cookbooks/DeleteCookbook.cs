3b6913664b2842b8f258baa8e47cecb462210d7b
Patryk <stepnikpa@gmail.com>
2025-08-27 21:29:51 +0200

    Delete cookbook

--- RecipeManager.API/Features/Cookbooks/DeleteCookbook.cs
+++ RecipeManager.API/Features/Cookbooks/DeleteCookbook.cs
@@ -1,5 +1,43 @@
-﻿namespace RecipeManager.Api.Features.Cookbooks;
+﻿using RecipeManager.Api.Application.Abstractions;
+using RecipeManager.Api.Application.Exceptions;
+using RecipeManager.Api.Domain.Cookbooks;
+using RecipeManager.Api.Shared.Contracts.Authorization;
+using RecipeManager.Api.Shared.Endpoint;
 
-public class DeleteCookbook
+namespace RecipeManager.Api.Features.Cookbooks;
+
+public static class DeleteCookbook
 {
+    public record struct Request(Guid Id) : IRequestId<Request>;
+
+    public sealed class AuthorizationPolicy(ICurrentUser currentUser, IAppDbContext dbContext) : IAuthorizationPolicy<Request>
+    {
+        public async Task<bool> IsAuthorized(Request request)
+        {
+            return dbContext.Cookbooks.Any(cookbook => cookbook.Id.Value == request.Id && cookbook.CreatedBy == currentUser.Id);
+        }
+    }
+
+    [GroupEndpoint("Cookbooks")]
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapStandardAuthenticatedDelete<AuthorizationPolicy, Request>("/{cookbookId}", Handler)
+                     .WithName("DeleteCookbook")
+                     .WithDescription("Deletes a cookbook");
+        }
+    }
+
+    public static async Task<IResult> Handler(Request cookbookId, IAppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        CookbookId id = new(cookbookId.Id);
+        Cookbook? cookbook = await dbContext.Cookbooks.FindAsync(new object[] { id }, cancellationToken)
+                          ?? throw new EntityNotFoundException<Cookbook, CookbookId>(id);
+
+        dbContext.Cookbooks.Remove(cookbook);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Results.NoContent();
+    }
 }
